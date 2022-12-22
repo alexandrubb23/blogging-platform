ae5ddb0354e932d3826c4405ed0426e2bb23fa52
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2022-12-22 23:50:57 +0200

    Implement tinymce.

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea.tinymce-editor ',
+        plugins: 'code table lists',
+        toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    });
+</script>
