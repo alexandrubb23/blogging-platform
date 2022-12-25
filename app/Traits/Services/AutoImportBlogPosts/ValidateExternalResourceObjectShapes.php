751ab73c9965caa7bf20e2a566a02c07f2db3ca3
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2022-12-25 19:33:38 +0200

    Refactor ValidateExternalResourceObjectShapes.

--- app/Traits/Services/AutoImportBlogPosts/ValidateExternalResourceObjectShapes.php
+++ app/Traits/Services/AutoImportBlogPosts/ValidateExternalResourceObjectShapes.php
@@ -5,27 +5,59 @@ namespace App\Traits\Services\AutoImportBlogPosts;
 trait ValidateExternalResourceObjectShapes
 {
     /**
+     * The expected shape of the external api result.
+     * 
+     * @var array
+     */
+    private $externalResourceShape = [
+        'status',
+        'articles'
+    ];
+
+    /**
+     * The expected shape of the external api article.
+     * 
+     * @var array
+     */
+    private $articleShape = [
+        'id',
+        'title',
+        'description',
+    ];
+
+    /**
      * Check if the external api result has the expected shape.
      * 
-     * @param object $externalApiResult
+     * @param object $result
      * @return bool
      */
-    protected function hasValidShape($externalApiResult): bool
+    protected function hasValidShape($result): bool
     {
-        return property_exists($externalApiResult, 'status')
-            && property_exists($externalApiResult, 'articles');
+        return $this->propertyExists($this->externalResourceShape, $result);
     }
 
     /**
      * Check if the external api articles has the expected shape.
      * 
-     * @param object $externalApiResult
+     * @param object $result
      * @return bool
      */
-    protected function hasValidArticlesShape(object $article): bool
+    protected function hasValidArticleShape(object $article): bool
     {
-        return property_exists($article, 'id')
-            && property_exists($article, 'title')
-            && property_exists($article, 'description');
+        return $this->propertyExists($this->articleShape, $article);
+    }
+
+    /**
+     * Check if the external api result has the expected shape.
+     * 
+     * @param object $result
+     * @return bool
+     */
+    private function propertyExists(array $properties, object $result)
+    {
+        foreach ($properties as $property)
+            if (!property_exists($result, $property)) return false;
+
+        return true;
     }
 }
