4e8d83e570ea7b40bc884d7bff8f2002278326bf
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-01-17 10:33:37 +0200

    Refactor up app bash script.
    This bash script is for lazy persons 😀 ¯\_(ツ)_/¯

--- up.sh
+++ up.sh
@@ -1,9 +1,15 @@
  #!/bin/bash
+
+# This bash script is for lazy persons 😀 ¯\_(ツ)_/¯
+
 # Create a type alias called sail
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 
-# Run the Database Migrations
-sail artisan migrate
+# Run the APP (in detach mode) ) 🚀
+sail up -d
 
-# Run the APP
-sail up
+# Run the Database Migration + seed 🛢
+sail artisan migrate:refresh --seed
+
+# Start auto importing 👷
+sail artisan schedule:work
