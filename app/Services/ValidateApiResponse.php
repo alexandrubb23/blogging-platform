5ab0fcf6e99939464b2011a3ef83fe78958d6507
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-01-17 10:00:55 +0200

    Implement ValidateApiResponse Service.

---
+++ app/Services/ValidateApiResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\ValidatorApiResponse;
+
+class ValidateApiResponse
+{
+    /**
+     * Validate the response.
+     *
+     * @param object $response
+     * @return boolean
+     */
+    public static function isValidApiResponse(string $api_url, object $response)
+    {
+        return ValidatorApiResponse::validate($api_url, $response, [
+            'status' => 'required|string|in:ok',
+            'articles' => 'required|array'
+        ]);
+    }
+
+    /**
+     * Validate the article.
+     *
+     * @param object $article
+     * @return boolean
+     */
+    public static function isValidApiArticle(string $api_url, object $article)
+    {
+        return ValidatorApiResponse::validate($api_url, $article, [
+            'id' => 'required|int',
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'publishedAt' => 'required|string'
+        ]);
+    }
+}
